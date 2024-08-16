620cf801016f76635129c5c695237a65a0d78796
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-17 01:16:27 +0200

    basic ui start

--- apps/app/src/routes/+layout.svelte
+++ apps/app/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '@kader/ui/styles.css';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
+<ModeWatcher />
 <slot />
+
+<div class="flex items-center justify-center h-screen">
+	<div class="h-20 rounded-3xl bg-background w-96 neu-up"></div>
+</div>
