d9f2774580add0d3dbf139fb6f521fb37256b787
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-17 09:43:28 +0200

    navbar anim

--- apps/app/src/routes/+layout.svelte
+++ apps/app/src/routes/+layout.svelte
@@ -1,11 +1,34 @@
 <script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import '@kader/ui/styles.css';
 	import { ModeWatcher } from 'mode-watcher';
 </script>
 
 <ModeWatcher />
-<slot />
 
 <div class="flex items-center justify-center h-screen">
-	<div class="h-20 rounded-3xl bg-background w-96 neu-up"></div>
+	<slot />
+
+	<div class="relative flex items-center justify-around h-20 rounded-3xl bg-background w-96 neu-up">
+		<div
+			class="absolute w-16 h-16 rounded-full bg-primary neu-r -z-10 duration-300 {$page.url.pathname.startsWith(
+				'/explore'
+			)
+				? 'left-8'
+				: $page.url.pathname.startsWith('/profile')
+					? 'left-72'
+					: 'left-40'}"
+		/>
+
+		<button on:click={() => goto('/explore')}>
+			<img class="w-6 h-6 m-6" src="/explore.svg" alt="" />
+		</button>
+		<button on:click={() => goto('/')}>
+			<img class="w-6 h-6 m-6" src="/logo.svg" alt="" />
+		</button>
+		<button on:click={() => goto('/profile')}>
+			<img class="w-6 h-6 m-6" src="/profile.svg" alt="" />
+		</button>
+	</div>
 </div>
