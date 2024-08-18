2f8b846906eafd78ef2af2368eb234be1da326e9
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-18 09:54:40 +0200

    uppy

---
+++ apps/app/src/routes/(app)/pfp/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Uppy } from '@uppy/core';
+	import Dashboard from '@uppy/dashboard';
+	import Webcam from '@uppy/webcam';
+	import { onMount } from 'svelte';
+
+	import '@uppy/core/dist/style.min.css';
+	import '@uppy/dashboard/dist/style.min.css';
+
+	onMount(() => {
+		new Uppy().use(Dashboard, { inline: true, target: '#uppy' }).use(Webcam);
+	});
+</script>
+
+<div id="uppy"></div>
